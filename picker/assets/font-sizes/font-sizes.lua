f169753c38a5353602a55cb76a09485aa9b0e467
tkykd <taogaoshen@outlook.com>
2024-10-31 09:47:30 +0800

    make it simple (return list of font sizes)

--- picker/assets/font-sizes/font-sizes.lua
+++ picker/assets/font-sizes/font-sizes.lua
@@ -5,12 +5,24 @@
 ---@class PickList
 local M = {}
 
+--[[
 M.get = function()
   local sizes = {}
   for i = 8, 30 do
     sizes[#sizes + 1] = { label = ("%2dpt"):format(i), id = tostring(i) }
   end
   sizes[#sizes + 1] = { id = tostring(require("config.font").font_size), label = "Reset" }
+
+  return sizes
+end
+]]
+--
+
+M.get = function()
+  local sizes = {}
+  for i = 8, 30 do
+    sizes[#sizes + 1] = { label = ("%2dpt"):format(i), id = tostring(i) }
+  end
 
   return sizes
 end
